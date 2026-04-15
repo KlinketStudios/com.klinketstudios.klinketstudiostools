a1a8e763d9cfa6f8ef06622ebf350ee9a91aa01b
KlinketStudios <KlinketStudios@gmail.com>
2026-04-15 02:14:17 -0700

    added auto importing TMP essentials

--- Editor/ProjectSetup.cs
+++ Editor/ProjectSetup.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using System.Collections.Generic;
 using System.IO;
+using TMPro.EditorUtilities;
 using UnityEditor;
 using UnityEditor.PackageManager.Requests;
 using UnityEditor.PackageManager;
@@ -12,41 +13,8 @@ using Task = System.Threading.Tasks.Task;
 public static class ProjectSetup 
 {
     
-    
-    [MenuItem("Tools/Setup/Import Essentials/Step 2")]
-    static void ImportEssentials()
-    {
-        Assets.ImportAsset("TimeScale Toolbar.unitypackage","bl4st/Editor ExtensionsUtilities");
-        Assets.ImportAsset("vHierarchy 2.unitypackage","kubacho lab/Editor ExtensionsUtilities");
-        Assets.ImportAsset("In-game Debug Console.unitypackage","yasirkula/ScriptingGUI");
-        Assets.ImportAsset("Smart Console.unitypackage","EdgarDev/Editor ExtensionsUtilities");
-        Assets.ImportAsset("Hot Reload Edit Code Without Compiling.unitypackage","The Naughty Cult/Editor ExtensionsUtilities");
-        Packages.InstallPackages(new[] { "com.unity.cinemachine" });
-    }
-    
-    [MenuItem("Tools/Setup/Import Essentials/Step 3")]
-    static void MoveImportedEssentials()
-    {
-        Folders.Create("AssetPackages");
-        Refresh();
-        Folders.Move("AssetPackages", "bl4st");
-        Folders.Move("AssetPackages", "vHierarchy");
-        Refresh();
-    }
-
-    [MenuItem("Tools/Setup/Import Packages/AI")]
-    public static void InstallPackages()
-    {
-        Packages.InstallPackages(new[]
-        {
-            "com.unity.ml-agents@4.0.1"
-        });
-        
-        Assets.ImportCustomLocalPackage("Assets/KlinketStudiosTools/Packages/MLAgents.unitypackage");
-    }
-
     [MenuItem("Tools/Setup/Import Essentials/Step 1")]
-    public static async void CreateFolders()
+    public static void CreateFolders()
     {
         Folders.Create(
             "Animations",
@@ -74,6 +42,42 @@ public static class ProjectSetup
         //MoveAsset("Packages/com.klinketstudios.klinketstudiostools/Assets/KlinketStudiosTools", "Assets/KlinketStudiosTools");
         Refresh();
     }
+    
+    [MenuItem("Tools/Setup/Import Essentials/Step 2")]
+    static void ImportEssentials()
+    {
+        Assets.ImportAsset("TimeScale Toolbar.unitypackage","bl4st/Editor ExtensionsUtilities");
+        Assets.ImportAsset("vHierarchy 2.unitypackage","kubacho lab/Editor ExtensionsUtilities");
+        Assets.ImportAsset("In-game Debug Console.unitypackage","yasirkula/ScriptingGUI");
+        Assets.ImportAsset("Smart Console.unitypackage","EdgarDev/Editor ExtensionsUtilities");
+        Assets.ImportAsset("Hot Reload Edit Code Without Compiling.unitypackage","The Naughty Cult/Editor ExtensionsUtilities");
+        Packages.InstallPackages(new[] { "com.unity.cinemachine" });
+        string packagePath = TMP_EditorUtility.packageFullPath + "/Package Resources/TMP Essential Resources.unitypackage";
+        ImportPackage(packagePath, false);
+    }
+    
+    [MenuItem("Tools/Setup/Import Essentials/Step 3")]
+    static void MoveImportedEssentials()
+    {
+        Folders.Create("AssetPackages");
+        Refresh();
+        Folders.Move("AssetPackages", "bl4st");
+        Folders.Move("AssetPackages", "vHierarchy");
+    
+        Refresh();
+    }
+
+    [MenuItem("Tools/Setup/Import Packages/AI")]
+    public static void InstallPackages()
+    {
+        Packages.InstallPackages(new[]
+        {
+            "com.unity.ml-agents@4.0.1"
+        });
+        
+        Assets.ImportCustomLocalPackage("Assets/KlinketStudiosTools/Packages/MLAgents.unitypackage");
+    }
+
     
     static class Assets
     {
